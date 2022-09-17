94d5589634d6efb3907246e0a32feea83608475d
DhruviDesai28 <77914897+DhruviDesai28@users.noreply.github.com>
2022-09-17 13:29:41 +0530

    Time: 49 ms (95.70%), Space: 13.9 MB (66.06%) - LeetHub

--- 1002-find-common-characters/1002-find-common-characters.py
+++ 1002-find-common-characters/1002-find-common-characters.py
@@ -1,29 +1,13 @@
 class Solution:
-    def commonChars(self, words: List[str]) -> List[str]:
-        d={};
-        for i in words[0]:
-            if i in d:
-                d[i]+=1
-            else:
-                d[i] = 1
-        for i in range(1, len(words)):
-            t = {}
-            for j in words[i]:
-                if j not in t:
-                    t[j]=1  
-                else:
-                    t[j]+=1
-            for i in d:
-                if i in t:
-                    d[i] = min(d[i],t[i])
-                else:
-                    d[i]=0
-        x = []
-        for i in d:
-            while d[i]!=0:
-                x.append(i)
-                d[i]-=1
-        return(x)
+    def commonChars(self, A: List[str]) -> List[str]:
+        if len(A)<2 : return A
+        alist = set(A[0])     # make a set from first string
+        res = []
+        for one in alist:
+            n = min([a_word.count(one) for a_word in A])     # count min frequency of every letter in every word
+            if n:    # if n>0 , we append this letter n times
+                res += [one]*n
+        return res
             
             
                 
